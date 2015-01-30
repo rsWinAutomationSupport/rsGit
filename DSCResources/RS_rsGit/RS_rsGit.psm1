d552741cfa9fd6e7d7fd3f9a942db5e8747d7120
Alexei Andreyev <alexei.andreyev@gmail.com>
2015-01-30 14:45:16 +0000

    Merge command syntax fix applied

--- DSCResources/RS_rsGit/RS_rsGit.psm1
+++ DSCResources/RS_rsGit/RS_rsGit.psm1
@@ -244,17 +244,17 @@ function Set-TargetResource
             if (($localCommit -ne $originCommit) -or (-not $RepoStatus.Contains("branch is up-to-date")))
             {
                 # merge remote changes if local is behind origin seems to not work very well during provisioning - need to investigate further
-                # $GitOutput = ExecGit "merge origin/$Branch"
-                $GitOutput = ExecGit "reset --hard origin/$branch"
+                $GitOutput = ExecGit "merge remotes/origin/$Branch"
+                #$GitOutput = ExecGit "reset --hard origin/$branch"
                 $RepoStatus = ExecGit "status"
 
                 if($Logging) 
                 {
-                    #Write-EventLog -LogName DevOps -Source $myLogSource -EntryType Warning -EventId 1000 -Message ("Repo: $Name`nLocal repo is behind origin/$Branch :`ngit merge origin/$Branch :`n $GitOutput") 
-                    Write-EventLog -LogName DevOps -Source $myLogSource -EntryType Warning -EventId 1000 -Message ("Repo: $Name`nLocal repo is behind origin/$Branch :`ngit reset --hard origin/$branch :`n $GitOutput") 
+                    Write-EventLog -LogName DevOps -Source $myLogSource -EntryType Warning -EventId 1000 -Message ("Repo: $Name`nLocal repo is behind origin/$Branch :`ngit merge remotes/origin/$Branch :`n $GitOutput") 
+                    #Write-EventLog -LogName DevOps -Source $myLogSource -EntryType Warning -EventId 1000 -Message ("Repo: $Name`nLocal repo is behind origin/$Branch :`ngit reset --hard origin/$branch :`n $GitOutput") 
                 }
-                #Write-Verbose "Local repo is behind origin/$Branch :`ngit merge origin/$branch :`n $GitOutput"
-                Write-Verbose "Local repo is behind origin/$Branch :`ngit reset --hard origin/$branch :`n $GitOutput"
+                Write-Verbose "Local repo is behind origin/$Branch :`ngit merge remotes/origin/$branch :`n $GitOutput"
+                #Write-Verbose "Local repo is behind origin/$Branch :`ngit reset --hard origin/$branch :`n $GitOutput"
             }
 
             if (-not ($RepoStatus.Contains("working directory clean")))
